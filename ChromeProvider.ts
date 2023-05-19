23264d09de96b346721d400a0bb557e8b729f96f
enisdenjo <badurinadenis@gmail.com>
2023-05-19 17:48:16 +0200

    test in browser and should work with meros

---
+++ ChromeProvider.ts
@@ -0,0 +1,67 @@
+import { Vitest, Awaitable } from "vitest";
+import { remote, Browser } from "webdriverio";
+
+interface BrowserProvider {
+  name: string;
+  getSupportedBrowsers(): readonly string[];
+  initialize(ctx: Vitest, options: { browser: string }): Awaitable<void>;
+  openPage(url: string): Awaitable<void>;
+  close(): Awaitable<void>;
+}
+
+export default class ChromeProvider implements BrowserProvider {
+  public name = "webdriverio";
+
+  private ctx!: Vitest;
+  private browserName!: string;
+  private browser: Browser | null = null;
+
+  getSupportedBrowsers() {
+    return ["chrome"];
+  }
+
+  async initialize(ctx: Vitest, { browser }: { browser: string }) {
+    if (!this.getSupportedBrowsers().includes(browser)) {
+      throw new Error(`Unsupported browser "${browser}"`);
+    }
+    this.ctx = ctx;
+    this.browserName = browser;
+  }
+
+  async openBrowser() {
+    if (!this.browser) {
+      this.browser = await remote({
+        logLevel: "error",
+        capabilities: {
+          browserName: this.browserName,
+          "goog:chromeOptions": {
+            args: [
+              // allow self signed certificates
+              "--allow-insecure-localhost",
+              // deactivate cors
+              "--disable-web-security",
+            ],
+            headless: this.ctx.config.browser.headless,
+          },
+        },
+      });
+    }
+    return this.browser;
+  }
+
+  async openPage(url: string) {
+    const browserInstance = await this.openBrowser();
+    await browserInstance.url(url);
+  }
+
+  async close() {
+    await Promise.all([
+      this.browser?.sessionId ? this.browser?.deleteSession?.() : null,
+    ]);
+  }
+
+  catchError(_cb: (error: Error) => Awaitable<void>) {
+    // we dont care about the provider error
+    return () => {};
+  }
+}
--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,16 @@
 import { it } from "vitest";
+import { meros } from "meros/browser";
 
-it("should", () => {
-  console.log("it does");
+it("should work with meros", async ({ expect }) => {
+  const parts = await fetch("https://localhost:58080").then(meros);
+
+  assertAsyncIterator(parts);
 });
+
+function assertAsyncIterator<T>(
+  val: unknown
+): asserts val is AsyncIterableIterator<T> {
+  if (!(typeof Object(val)[Symbol.asyncIterator] === "function")) {
+    throw new Error("Value is not async iterable");
+  }
+}
